069146363b2e64a8c9ea69d0a02aeb7d0c11cb40
Christopher Dafnis <chris.dafnis@dakotais.co.uk>
2018-02-01 16:22:25 +0000

    Initial commit

---
+++ ObservableCollectionDemo/Views/FirstPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace ObservableCollectionDemo
+{
+	public partial class FirstPage : ContentPage
+	{
+		public static ObservableCollection<Items> objSecondopageList;
+
+		public FirstPage()
+		{
+			InitializeComponent();
+
+			objSecondopageList = new ObservableCollection<Items>();
+
+			NavigationPage.SetHasNavigationBar(this, false);
+			EmployeeView.ItemsSource = objSecondopageList;
+		}
+
+		private async void BtnAdd_Click(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new SecondPage());
+		}
+	}
+}
---
+++ ObservableCollectionDemo/Views/SecondPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace ObservableCollectionDemo
+{
+	public partial class SecondPage : ContentPage
+	{
+
+		public SecondPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void BtnSave_Click(object sender, EventArgs e)
+		{
+			FirstPage.objSecondopageList.Add(new Items
+			{
+				FirstName = entryfirstname.Text,
+				LastName = entrylastname.Text,
+				CompanyName = entrycomapanyname.Text,
+				MobileNumber = entrymobilenumber.Text,
+				Designation = entrydesignation.Text,
+				Address = editoraddress.Text
+			});
+
+			await Navigation.PopAsync();
+		}
+
+		void mobileNoChanged(object sender, TextChangedEventArgs e)
+		{
+			var mobileObject = (Entry)sender;
+			var textValue = mobileObject.Text;
+			if (textValue.Length > 10)
+			{
+				entrymobilenumber.Text = textValue.Remove(textValue.Length - 1);
+			}
+		}
+	}
+
+	public class Items
+	{
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string CompanyName { get; set; }
+
+		public string MobileNumber { get; set; }
+
+		public string Designation { get; set; }
+
+		public string Address { get; set; }
+	}
+}
